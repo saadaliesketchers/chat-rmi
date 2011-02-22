ed33e1d63ce6bfc63611c6b1bdd3e7e4f49e4ebf
iegorov.oleg <iegorov.oleg@21bce8b9-a469-b45e-006d-65dbc4d7296d>
2011-02-22 11:38:48 +0000

    added comments

--- ChatClient.java
+++ ChatClient.java
@@ -2,6 +2,15 @@ import java.rmi.*;
 import java.util.*;
 
 public interface ChatClient extends Remote {
+  //----------------Receive--------------------
+  //  display information to the user
+  //  type of information is specified by the 'mode' flag:
+  //  's' -- someone said something
+  //  'e' -- someone has entered the chat
+  //  'l' -- someone has left the chat
+  //  'h' -- display the history
 	public void receive(String message, String who, char mode) throws RemoteException;
+  
+  //return the name of user
   public String GetName() throws RemoteException; 
 }
--- ChatClientImplement.java
+++ ChatClientImplement.java
@@ -14,27 +14,35 @@ public class ChatClientImplement extends UnicastRemoteObject implements ChatClie
       return client_name;
     }
 
+
     public ChatClientImplement(ChatServer serv) throws RemoteException {
         super();
         cs = serv;
+        
+        //before the user enters chat -- prompt to specify the name
         System.out.println("Enter your name, please: ");
         Scanner forname = new Scanner(System.in);
         client_name = forname.nextLine();
         
+        //connect user to the chat
         cs.Connect(this);
     }
 
     public synchronized void receive(String message, String who, char mode) throws RemoteException {
       switch(mode) {
+        //if 'who' user has said 'message'
         case 's':
           System.out.println (who + " says : " + message);
           break;
+        //if 'who' user has entered the chat
         case 'e':
           System.out.println (who + " entered the chat");
           break;
+        //if 'who' user has left the chat
         case 'l':
           System.out.println (who + " left the chat");
           break;
+        //if the user just came -- display 
         case 'h':
           System.out.println (message);
           break;
@@ -47,11 +55,17 @@ public class ChatClientImplement extends UnicastRemoteObject implements ChatClie
 
       while(true) {
         try {
+          //get next input from user
           msg = in.nextLine();
+          
+          //if user typed 'exit' -- to leave the chat
           if (msg.equals("exit")) {
+            //disconnect him
             cs.Disconnect(this);
             break;
           }
+          
+          //send the entered message to everyone
           cs.SendMessage(msg, this.GetName(), 's');
         }  
         catch (Exception e) {
